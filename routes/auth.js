8d2aedfbef694e63bfad74a72f2dd25549535a14
Stelios Taliadwros <steliosevoix@gmail.com>
2024-06-29 16:17:04 +0300

    feat: adding request limiter middleware to the request reset password route

--- routes/auth.js
+++ routes/auth.js
@@ -1,10 +1,16 @@
 const express = require("express");
+const { reqPassLimiter } = require("../middleware/requestPasswordLimiter");
 const router = express.Router();
 
-const { signUp, signIn, requestResetPassword, resetPassword } = require("../controllers/auth");
+const {
+  signUp,
+  signIn,
+  requestResetPassword,
+  resetPassword,
+} = require("../controllers/auth");
 
 router.post("/sign-up", signUp);
 router.post("/sign-in", signIn);
-router.post("/request-reset-password", requestResetPassword);
-router.post("/reset-password", resetPassword)
+router.post("/request-reset-password", reqPassLimiter, requestResetPassword);
+router.post("/reset-password", resetPassword);
 module.exports = router;
