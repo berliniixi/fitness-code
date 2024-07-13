ae632742bb1c876f684fc4d02c70bf3dfffc17bc
Stelios Taliadwros <steliosevoix@gmail.com>
2024-07-13 12:34:05 +0300

    feat: controllers for every route in calendar

--- controllers/calendar.js
+++ controllers/calendar.js
@@ -1,45 +1,13 @@
 const Calendar = require("../models/Calendar");
-// const User = require("../models/User");
+const User = require("../models/User");
 const { StatusCodes } = require("http-status-codes");
+const formattedTime = require("../utils/dates/formatTime");
+const getBookedSessions = require("../utils/findBookedSessionPerUser");
 
 const bookSession = async (req, res) => {
   const userId = req.user.userId; // Assuming user is authenticated and user ID is available in req.user
 
   try {
-    // Check if the user is already book a session for today
-
-    // Check if the user has already booked 3 times in the same week
-    // const oneWeekAgo = new Date();
-    // oneWeekAgo.setDate(oneWeekAgo.getDate() - 7);
-
-    // const userBookings = await Calendar.countDocuments({
-    //   bookedBy: userId,
-    //   bookDate: { $gte: oneWeekAgo },
-    // });
-
-    // if (userBookings >= 3) {
-    //   return res.status(StatusCodes.BAD_REQUEST).json({
-    //     success: false,
-    //     message: "You have already booked 3 sessions this week.",
-    //   });
-    // }
-
-    // // Check if there are already 5 bookings for the given hour
-    // const start = new Date(bookDate);
-    // const end = new Date(bookDate);
-    // end.setHours(end.getHours() + 1); // session duration is 1 hour
-
-    // const overlappingBookings = await Calendar.countDocuments({
-    //   bookDate: { $lt: end },
-    // });
-
-    // if (overlappingBookings >= 5) {
-    //   return res.status(StatusCodes.BAD_REQUEST).json({
-    //     success: false,
-    //     message: "This time slot is fully booked. Please choose another time.",
-    //   });
-    // }
-
     // Create booking
     const booking = new Calendar({
       bookDate: new Date(req.body.bookDate),
@@ -69,8 +37,163 @@ const bookSession = async (req, res) => {
   }
 };
 
-const updateBookedSession = async (req, res) => {};
+const updateBookedSession = async (req, res) => {
+  const userId = req.user.userId; // Assuming user is authenticated and user ID is available in req.user
+  const bookingSessionId = req.params.id;
 
-const deleteBookedSession = async (req, res) => {};
+  // Compute bookFinishAt as one hour after bookStartAt
+  const [hours, minutes] = req.body.bookStartAt.split(":").map(Number);
+  const startDate = new Date(req.body.bookDate);
+  startDate.setHours(hours, minutes, 0, 0);
 
-module.exports = { bookSession };
+  // Set bookFinishAt to one hour after bookStartAt
+  const finishDate = new Date(startDate.getTime() + 60 * 60 * 1000);
+
+  req.body.bookFinishAt = formattedTime(finishDate);
+
+  try {
+    const updatedBooking = await Calendar.findByIdAndUpdate(
+      { _id: bookingSessionId, bookedBy: userId },
+      { ...req.body, bookFinishAt: req.body.bookFinishAt },
+      { new: true, runValidators: true }
+    );
+
+    if (!updatedBooking) {
+      res
+        .status(StatusCodes.OK)
+        .json({ success: false, error: ["There is not booked session"] });
+    }
+
+    res.status(StatusCodes.OK).json({ success: true, updatedBooking });
+  } catch (error) {
+    console.log(error);
+    if (error.name === "ValidationError") {
+      const errors = Object.values(error.errors).map((err) => err.message);
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ success: false, errors: errors });
+    }
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ success: false, error });
+  }
+};
+
+const deleteBookedSession = async (req, res) => {
+  const bookingSessionId = req.params.id;
+  const userId = req.user.userId; // Assuming user ID is stored in req.user
+
+  try {
+    const existingBooking = await Calendar.findOne({ _id: bookingSessionId });
+
+    console.log("existingBooking: ", existingBooking);
+    if (!existingBooking) {
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ success: false, error: ["Couldn't find the booked session"] });
+    }
+
+    // Check if the user making the request is the same as the one who created the booking
+    if (existingBooking.bookedBy.toString() !== userId) {
+      return res.status(StatusCodes.FORBIDDEN).json({
+        success: false,
+        error: ["You are not authorized to delete this booking"],
+      });
+    }
+
+    await Calendar.findOneAndDelete({
+      _id: bookingSessionId,
+      bookedBy: userId,
+    });
+
+    res.status(StatusCodes.OK).json({
+      success: true,
+      message: `Booked Session at ${
+        existingBooking.bookDate.toISOString().split("T")[0]
+      } ${existingBooking.bookStartAt} deleted successfully.`,
+    });
+  } catch (error) {
+    console.log("from here", error);
+    if (error.name === "ValidationError") {
+      const errors = Object.values(error.errors).map((err) => err.message);
+      return res
+        .status(StatusCodes.BAD_REQUEST)
+        .json({ success: false, errors: errors });
+    }
+
+    if (error.name === "CastError") {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        success: false,
+        errors: [
+          `Couldn't find such booked session with id '${bookingSessionId}'.`,
+        ],
+      });
+    }
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ success: false, error });
+  }
+};
+
+const getAllBookedSessions = async (req, res) => {
+  try {
+    const bookedSessions = await Calendar.find().sort("bookDate");
+    return res.status(StatusCodes.OK).json({
+      success: true,
+      bookedSessions,
+      length: bookedSessions.length,
+    });
+  } catch (error) {
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ success: false, error });
+  }
+};
+
+const getByUserBookedSessions = async (req, res) => {
+  try {
+    const user = await User.findOne({ _id: req.params.userId });
+    const userBookedSessions = await getBookedSessions(req.params.userId);
+
+    console.log("userBookedSessions: ", userBookedSessions);
+
+    if (!user) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        success: false,
+        error: ["User does not exists"],
+      });
+    }
+
+    if (!userBookedSessions) {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        success: false,
+        error: ["Requested users booked sessions couldn't be found."],
+        length: userBookedSessions.length,
+      });
+    }
+
+    return res.status(StatusCodes.OK).json({
+      success: true,
+      userBookedSessions,
+      length: userBookedSessions.length,
+    });
+  } catch (error) {
+    if (error.name === "CastError") {
+      return res.status(StatusCodes.BAD_REQUEST).json({
+        success: false,
+        errors: [`Couldn't find such an ${req.params.userId}.`],
+      });
+    }
+    res
+      .status(StatusCodes.INTERNAL_SERVER_ERROR)
+      .json({ success: false, error });
+  }
+};
+
+module.exports = {
+  bookSession,
+  updateBookedSession,
+  deleteBookedSession,
+  getAllBookedSessions,
+  getByUserBookedSessions,
+};
