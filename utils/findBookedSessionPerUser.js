25c8aa99d278d69797d7e0dc0edd8d2ce93b6859
Stelios Taliadwros <steliosevoix@gmail.com>
2024-07-13 12:27:59 +0300

    feat: getting all booked sessions for a specific user

---
+++ utils/findBookedSessionPerUser.js
@@ -0,0 +1,17 @@
+const Calendar = require("../models/Calendar");
+// Function that get all the booked session of user
+const getBookedSessions = async (userId) => {
+  try {
+    const query = {
+      bookedBy: userId,
+    };
+
+    const bookedSessions = await Calendar.find(query)
+
+    return bookedSessions;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+module.exports = getBookedSessions;
