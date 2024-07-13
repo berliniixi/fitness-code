4f3185e4a59e7bdad95894540bbd0106b8a850f2
Stelios Taliadwros <steliosevoix@gmail.com>
2024-07-13 12:35:02 +0300

    feat: routes for every case in calendar route

--- routes/calendar.js
+++ routes/calendar.js
@@ -1,16 +1,21 @@
 const express = require("express");
 const router = express.Router();
 
-const { bookSession } = require("../controllers/calendar");
-// create
+const {
+  bookSession,
+  updateBookedSession,
+  deleteBookedSession,
+  getAllBookedSessions,
+  getByUserBookedSessions,
+} = require("../controllers/calendar");
 const isUserActive = require("../middleware/Booking/checkUserActivation");
 const isBookDatePresent = require("../middleware/Booking/checkBookDate");
 const checkMultipleBookingsPerUser = require("../middleware/Booking/checkMultipleBookingsPerUser");
 const checkMultipleBookingsPerWeek = require("../middleware/Booking/checkMultipleBookingPerWeek");
 const checkMultipleBookingsPerHour = require("../middleware/Booking/checkMultipleBookingsPerHour");
-// update - modified
+const isUpdatingTheSameSession = require("../middleware/Booking/checkUpdateBookSession");
 
-// delete
+// create
 router.post(
   "/create",
   [
@@ -22,4 +27,26 @@ router.post(
   ],
   bookSession
 );
+
+// update - modified
+router.patch(
+  "/update/:id",
+  [
+    isUserActive,
+    isBookDatePresent,
+    isUpdatingTheSameSession,
+    // checkMultipleBookingsPerUser,
+    checkMultipleBookingsPerHour,
+    checkMultipleBookingsPerWeek,
+  ],
+  updateBookedSession
+);
+// delete
+router.delete("/delete/:id", deleteBookedSession);
+
+// Get all booked sessions from all users
+router.get("/get-all", getAllBookedSessions);
+
+// Get all booked sessions from a user
+router.get("/get-all/:userId", getByUserBookedSessions);
 module.exports = router;
