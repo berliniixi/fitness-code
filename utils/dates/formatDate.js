61c2d9da39d6dca5c46b4b3053a8bb8905eddc73
Stelios Taliadwros <steliosevoix@gmail.com>
2024-07-13 12:07:24 +0300

    reafactor: update format date functionality to return only the date

--- utils/dates/formatDate.js
+++ utils/dates/formatDate.js
@@ -1,23 +1,18 @@
 const formattedDate = (date) => {
 
-  const hours = String(date.getHours()).padStart(2, "0");
-  const minutes = String(date.getMinutes()).padStart(2, "0");
-  const seconds = String(date.getSeconds()).padStart(2, "0");
-
   // Get the year, month, and day components separately
   const year = date.getFullYear();
   const month = date.getMonth() + 1; // Month is zero-based, so add 1
   const day = date.getDate();
 
   // Construct the date string in the desired format (DD-MM-YYYY)
-  const formattedDate = `${day < 10 ? "0" + day : day}-${
-    month < 10 ? "0" + month : month
-  }-${year}`;
-  const formattedTime = `${hours}:${minutes}:${seconds}`;
-  const dayTime = `${formattedDate} - ${formattedTime}`;
+  const formattedDate = `${year}-${month < 10 ? "0" + month : month}-${
+    day < 10 ? "0" + day : day
+  }`;
+  // const formattedTime = `${hours}:${minutes}:${seconds}`;
+  const dayTime = `${formattedDate}`;
 
   return dayTime;
-
 };
 
-module.exports = { formattedDate };
+module.exports = formattedDate;
